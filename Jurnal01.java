32de608aaf6106ad53ba5dbf2064e4609dedcdbb
Fauzan <fauzannurdiansyah13@gmail.com>
2023-09-28 12:04:43 +0700

    first commit

---
+++ Jurnal01.java
@@ -0,0 +1,12 @@
+public class Jurnal01 {
+    public static void main(String[] args) {
+        String nama = "Fauzan Nurdiansyah";
+        String kelas = "D3IF-47-03";
+        String nim = "607062300111";
+
+        System.out.println("Nama:"+nama);
+        System.out.println("Kelas:"+kelas);
+        System.out.println("NIM:"+nim);
+    }
+    
+}
